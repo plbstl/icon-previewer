7556ad107cecc52d93e47d04932407ceec6f6990
Paul Ebose <paulebose@gmail.com>
2023-06-17 10:30:13 +0100

    implement ui to show list of icons

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello you!')
+import Masonry from './masonry'
+
+const masonry = new Masonry({
+  container: '.cards',
+  baseWidth: 128,
+  surroundingGutter: false,
+  gutter: 24,
+  ultimateGutter: 16,
+})
+
+// img.clientHeight affects masonry layout
+const hardcodedSelector = 'body > main > .cards > .card:nth-child(5) > .preview > img'
+;(document.querySelector(hardcodedSelector) as HTMLImageElement).onload = masonry.layout
